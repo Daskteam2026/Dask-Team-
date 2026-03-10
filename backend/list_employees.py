9284998dac6c433190b4590620ebf0245a52acf4
Ashutosh Rout <ashurout66@gmail.com>
2026-03-10 23:02:55 +0530

    Delete backend/list_employees.py

--- backend/list_employees.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import sessionmaker
-from .database import engine, Employee
-
-Session = sessionmaker(bind=engine)
-s = Session()
-for e in s.query(Employee).all():
-    print(e.id, e.name, e.department, e.join_date)
